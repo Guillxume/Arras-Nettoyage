d0681d9a3989a246cba986cc325a41f9ec16952a
Guillxume <58263104+Guillxume@users.noreply.github.com>
2020-10-22 11:29:54 +0200

    Add files via upload

--- Siteweb/views/footer.php
+++ Siteweb/views/footer.php
@@ -1,3 +1,33 @@
+
+        <footer class="page-footer light-blue lighten-3">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Coordonnées</h5>
+                <p class="grey-text text-lighten-4">06.08.84.94.19</p>
+                <p class="grey-text text-lighten-4">arras.nettoyage@orange.fr</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Liens externes</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="http://facebook.com/arrasnettoyage/">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="http://pagesjaunes.fr/arrasnettoyage">Pages Jaunes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2020 Léviathan Productions
+            <a class="grey-text text-lighten-4 right" href="#!">Porte-Folio</a>
+            </div>
+          </div>
+        </footer>
+            
+<!-- Appel du Javascript distant et local !-->
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="src/assets/js/script.js"></script>
+</body>
+</html>
